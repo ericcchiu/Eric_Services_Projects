472ca4785e358ce07f728ec7d2dd9f49455aca7a
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-06 17:28:52 -0600

    (styling) Update alignment and borders.

--- client/src/components/display_components/ProjectView.js
+++ client/src/components/display_components/ProjectView.js
@@ -1,22 +1,27 @@
 import React from "react";
 import "./ProjectView.css";
+
+import Grid from "@material-ui/core/Grid";
+
 const ProjectView = props => {
   return (
     <div className="ProjectView">
-      <div className="CreatorInfoDisplay">
-        <img
-          src={props.currentProject.creator_img}
-          alt="https://cdn2-www.dogtime.com/assets/uploads/gallery/samoyed-dogs-and-puppies/samoyed-dogs-puppies-1.jpg"
-        />
-        <div>
-          <span>By {props.currentProject.creator}</span>
+      <Grid direction="row" justify="center" alignItems="center">
+        <div className="CreatorInfoDisplay">
+          <img
+            src={props.currentProject.creator_img}
+            alt="https://cdn2-www.dogtime.com/assets/uploads/gallery/samoyed-dogs-and-puppies/samoyed-dogs-puppies-1.jpg"
+          />
+          <div>
+            <span>By {props.currentProject.creator}</span>
+          </div>
         </div>
-      </div>
 
-      <div className="ProjectHeader">
-        <h1>{props.currentProject.name}</h1>
-        <h3>{props.currentProject.blurb}</h3>
-      </div>
+        <div className="ProjectHeader">
+          <h1>{props.currentProject.name}</h1>
+          <h3>{props.currentProject.blurb}</h3>
+        </div>
+      </Grid>
 
       <div className="ProjectImage">
         <div className="full-project-image">
