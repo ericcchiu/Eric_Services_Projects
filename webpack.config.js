e641b3c4853e0af1b9d12e83525a4c6dc1dbbe21
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-29 16:19:14 -0600

    (bugfix) Fix issue where CSS modules option was hashing the classname.

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,7 @@ module.exports = {
         loader: [
           "style-loader",
           {
-            loader: "css-loader",
-            options: {
-              modules: true
-            }
+            loader: "css-loader"
           }
         ]
       }
